89af97e83730b4cc46c2d298392f9abff45ed409
marcelo <marontbal@gmail.com>
2025-07-30 12:15:42 -0600

    Render Posts

--- index.js
+++ index.js
@@ -24,17 +24,50 @@ const getCommentsForEachPost = async (posts) => {
   postComments.forEach((comments, i) => posts[i].comments = comments);
 }
 
+const renderHtml = (user, posts) => {
+  const content = document.getElementById('content');
+  content.innerHTML += `<h3>Posts del usuario ${user.email}</h3>`;
+  
+  posts.forEach(post => {
+    content.innerHTML += `
+    <div class="post">
+      <h4>${post.title}</h4>
+      <p>${post.body}</p>
+      <br>
+      ${post.comments.map(c => `<p><span>${c.email}:</span>${c.body}</p>`).join('')}
+    </div>
+    `;
+  })
+}
+
 const getBlogContent = async () => {
   try {
     const user = await getUser(1);
     const posts = await getPosts(user);
     await getCommentsForEachPost(posts);
 
-    console.log(user);
-    console.log(posts)
+    renderHtml(user, posts);
   } catch (err) {
     console.log(err);
   }
 }
 
 getBlogContent();
+
+const loadAdds = () => {
+  console.log('Adds loaded');
+}
+
+const affiliateRedirect = () => {
+  // resolver el bug
+}
+
+// test commit
+
+// cambio 1
+// cambio 2
+// cambio 3
+
+// cambios videos 1
+// cambios videos 2
+// cambios videos 3
